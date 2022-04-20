bcec2b51a8fe65b569b216af7b819ff24fd50894
Antoinette Munoz <93494243+Antiimoon@users.noreply.github.com>
2022-04-20 14:07:42 -0700

    some styling

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -43,8 +43,8 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
+          <span role="img" aria-label="shopping bag">📚</span>
+          The Book Club
         </Link>
       </h1>
 
